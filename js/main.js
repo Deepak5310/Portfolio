dd36c08cf7667de4cc3c43d9c43a62afd0e06716
deepak5310 <deepakjangir5310@gmail.com>
2021-12-23 00:58:46 +0530

    js: fix offsetHeight
    Signed-off-by: deepak5310 <deepakjangir5310@gmail.com>

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ function scrollActive(){
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
-        sectionId = current('id')
+        sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
